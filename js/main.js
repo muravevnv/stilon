545d2c150d7d3a9e13d5fc789c8c9f9222e89533
Nick Muravev <murvievnick@gmail.com>
2026-06-06 17:02:29 +0300

    feat: каталог категории, меню в шапке

--- js/main.js
+++ js/main.js
@@ -6,21 +6,42 @@ document.addEventListener('DOMContentLoaded', () => {
         window.addEventListener('scroll', () => {
 
             if (window.scrollY > 0) {
-                header.classList.add('is-white');
+                header.classList.add('header--white');
             } else {
-                header.classList.remove('is-white');
+                header.classList.remove('header--white');
             }
         });
     }
 
     initStickyHeader();
 
-    function initMap() {
-        const map = document.querySelectorAll('.js-map');
 
-        if(map.length > 0) {
+    function initHeaderCatalog() {
+        const header = document.querySelector('.header');
+        const headerCatalogLink = document.querySelector('.js-header-catalog-link');
+        const headerCatalogMenu = document.querySelector('.js-header-catalog-menu');
+        
+        let closeTimeout;
+
+        const openCatalog = () => {
+            clearTimeout(closeTimeout); 
+            header.classList.add('header--open-catalog');
+        };
+
+        const closeCatalog = () => {
+            clearTimeout(closeTimeout); 
             
-        }
+            closeTimeout = setTimeout(() => {
+                header.classList.remove('header--open-catalog');
+            }, 500);
+        };
+
+        headerCatalogLink.addEventListener('mouseenter', openCatalog);
+        headerCatalogLink.addEventListener('mouseleave', closeCatalog);
+
+        headerCatalogMenu.addEventListener('mouseenter', openCatalog);
+        headerCatalogMenu.addEventListener('mouseleave', closeCatalog);
     }
 
+    initHeaderCatalog();
 })
